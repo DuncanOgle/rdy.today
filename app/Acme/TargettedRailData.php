14a33a2630ed35905a40ce6c0f8c56de0763b4ba
Duncan Ogle <duncan.ogle@gmail.com>
2019-02-17 16:06:14 +0000

    Update TargettedRailData.php

--- app/Acme/TargettedRailData.php
+++ app/Acme/TargettedRailData.php
@@ -12,7 +12,7 @@ use App\Acme\RailData;
 
 class TargettedRailData {
     static function lines() {
-        $approvedStationList = ["LEW","LBG","CHX","CST","WAT","WAE","GTW","TBD","BTN","EUS","MAN","BON","CTR","FLN", "GNW"];
+        $approvedStationList = ["LEW","LBG","CHX","CST","WAT","WAE","GTW","TBD","BTN","EUS","MAN","BON","CTR","FLN","GNW","BKH","DFD","LAD"];
 
         $approved = array_filter(RailData::lines(), function ($element) use ($approvedStationList) {
             return in_array($element, $approvedStationList);
